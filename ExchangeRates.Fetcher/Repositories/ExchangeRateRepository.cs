3146ec3e56cc2ed411623256c373a74e4736b70c
lucian <domenkirnkrefl@icloud.com>
2024-11-19 10:19:25 +0100

    feat: add implementations for interfaces

---
+++ ExchangeRates.Fetcher/Repositories/ExchangeRateRepository.cs
@@ -0,0 +1,31 @@
+﻿using ExchangeRates.Shared.Interfaces;
+using ExchangeRates.Shared.Models;
+using Npgsql;
+
+namespace ExchangeRates.Fetcher.Repositories
+{
+    public class ExchangeRateRepository
+    {
+        public ExchangeRateRepository(IConnectionStrings connectionStrings)
+        {
+            _connectionString = connectionStrings.Postgres;
+        }
+
+        private readonly string _connectionString;
+
+        public async Task SaveExchangeRateAsync(ExchangeRate exchangeRate)
+        {
+            await using var connection = new NpgsqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            var command = new NpgsqlCommand(
+                "INSERT INTO ExchangeRates (Currency, Rate, Date) VALUES (@Currency, @Rate, @Date)", connection);
+
+            command.Parameters.AddWithValue("Currency", exchangeRate.CurrencyCode);
+            command.Parameters.AddWithValue("Rate", exchangeRate.Rate);
+            command.Parameters.AddWithValue("Date", exchangeRate.Date);
+
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+}
---
+++ ExchangeRates.Fetcher/Services/FetcherService.cs
@@ -0,0 +1,26 @@
+﻿using ExchangeRates.Fetcher.Interfaces;
+
+namespace ExchangeRates.Fetcher.Services
+{
+    internal class FetcherService : IFetcherService
+    {
+        public FetcherService(IHttpClientService httpClientInterface, IExchangeRateRepository exchangeRateRepository)
+        {
+            _httpClient = httpClientInterface;
+            _exchangeRateRepository = exchangeRateRepository;
+        }
+
+        private readonly IHttpClientService _httpClient;
+        private readonly IExchangeRateRepository _exchangeRateRepository;
+
+        public async Task FetchAndSaveExchangeRatesAsync()
+        {
+            var exchangeRates = await _httpClient.GetExchangeRatesAsync();
+
+            foreach (var rate in exchangeRates)
+            {
+                await _exchangeRateRepository.SaveExchangeRateAsync(rate);
+            }
+        }
+    }
+}
