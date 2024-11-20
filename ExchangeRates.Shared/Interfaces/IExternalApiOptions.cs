65b4ed569b876b660e9cc751ff7cdabc96357cd2
lucian <domenkirnkrefl@icloud.com>
2024-11-20 11:19:07 +0100

    feat: add apiKey to external api options

--- ExchangeRates.Shared/Interfaces/IExternalApiOptions.cs
+++ ExchangeRates.Shared/Interfaces/IExternalApiOptions.cs
@@ -3,5 +3,6 @@
     public interface IExternalApiOptions
     {
         string Url { get; set; }
+        string ApiKey { get; set; }
     }
 }
--- ExchangeRates.Shared/Models/ExternalApiOptions.cs
+++ ExchangeRates.Shared/Models/ExternalApiOptions.cs
@@ -1,7 +1,10 @@
-﻿namespace ExchangeRates.Shared.Models
+﻿using ExchangeRates.Shared.Interfaces;
+
+namespace ExchangeRates.Shared.Models
 {
-    public class ExternalApiOptions
+    public class ExternalApiOptions : IExternalApiOptions
     {
         public required string Url { get; set; }
+        public required string ApiKey { get; set; }
     }
 }
