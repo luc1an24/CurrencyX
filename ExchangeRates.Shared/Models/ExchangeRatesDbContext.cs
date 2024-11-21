5fa9c2da74825369486632b31dd4674dcff2f9e9
lucian <domenkirnkrefl@icloud.com>
2024-11-21 19:22:33 +0100

    feat: add paged response to shared model

---
+++ ExchangeRates.Shared/Models/ExchangeRatesDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExchangeRates.Shared.Models
+{
+    public class ExchangeRatesDbContext : DbContext
+    {
+        public ExchangeRatesDbContext(DbContextOptions<ExchangeRatesDbContext> options) : base(options) { }
+
+        public DbSet<ExchangeRate> ExchangeRates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExchangeRate>()
+                .HasKey(e => new { e.CurrencyCode, e.Date });
+
+            modelBuilder.Entity<ExchangeRate>()
+                .Property(e => e.CurrencyCode)
+                .IsRequired();
+
+            modelBuilder.Entity<ExchangeRate>()
+                .Property(e => e.Date)
+                .IsRequired();
+        }
+    }
+}
---
+++ ExchangeRates.Shared/Models/PagedResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace ExchangeRates.Shared.Models
+{
+    public class PagedResponse<T>
+    {
+        public PagedResponse(IEnumerable<T> data, int pageNumber, int pageSize, int totalRecords)
+        {
+            Data = data;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalRecords = totalRecords;
+            TotalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+        }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+        public IEnumerable<T> Data { get; set; }
+    }
+}
